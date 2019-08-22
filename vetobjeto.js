f7de525dc63f669cd3ac9acb0ab1c76007d3e493
patykastros <patykastros@gmail.com>
2019-08-21 21:48:46 -0300

    Vetor de Objetos V.1

---
+++ vetobjeto.js
@@ -0,0 +1,15 @@
+function funcao(){
+    var vet = []
+    for(var i=0; i<5; i++){
+        var objeto ={}
+
+    //coloca dados no objeto
+    objeto.codigo = Number(prompt("Código"))
+    objeto.nome = Number(prompt("Nome"))
+    objeto.qtde = Number(prompt("Quantidade"))
+    objeto.preco = Number(prompt("Preço"))
+
+    //coloca objeto no vetor
+    vet.push(objeto)
+    }
+}
